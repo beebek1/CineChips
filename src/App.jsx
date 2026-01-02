48036c6d54cdaf7e30e8759ddfdb8e4a4616979a
beebek1 <beebek990@gmail.com>
2026-01-03 01:19:56 +0545

    added sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import SignUp from './pages/signup'
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
-
+import SignIn from './pages/Signin';
 
 function AppWrapper() {
   const location = useLocation();
-  const hideNavbar = location.pathname === '/signup';
+  const hideNavbar = location.pathname === '/signup' || location.pathname ==='/signin';
 
   return (
     <>
       {!hideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/signin" element={<SignIn />}/>
       </Routes>
     </>
   );
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,7 @@
+
 import React, { useState } from 'react';
 import moviePoster from '../assets/moviePoster.png';
 import { Link } from 'react-router-dom';
-
-
 
 
 export default function SignupIn() {
@@ -12,18 +11,12 @@ export default function SignupIn() {
 
   const[formData, setFormData] =useState({
     email : '',
-    fullName : '',
     password : '',
-    confirmPassword : '',
   })
 
   const handleSubmit = () => {
-    if(!formData.email || !formData.fullName || !formData.password || !formData.confirmPassword ){
-      alert("please fill all the fields")
-    }else if(formData.password !== formData.confirmPassword){
-      alert("confirm password didn't match")
-    }else if(!agreedToTerms){
-      alert("kindly agree to the terms and condition")
+    if(!formData.email || !formData.password){
+      return alert("please fill all the fields")
     }
     console.log(formData)
   };
@@ -54,27 +47,13 @@ export default function SignupIn() {
         <div className="w-full lg:w-1/2 flex items-center justify-center p-8 bg-white overflow-hidden">
           <div className="w-full max-w-md">
             <div className="text-center mb-8">
-              <h1 className="text-4xl font-bold text-gray-900 mb-2" style={{ fontFamily: 'MuseoModerno'  }} >Sign up to CineChips</h1>
+              <h1 className="text-4xl font-bold text-gray-900 mb-2" style={{ fontFamily: 'MuseoModerno'  }} >Sign in to CineChips</h1>
               <p className="text-gray-600 text-sm">Where Every Movie Feels Like the First Time</p>
             </div>
 
 
             {/* Form Fields */}
             <div className="space-y-5">
-              {/* Full Name */}
-              <div>
-                <label className="block text-xs font-medium text-gray-700 mb-2 uppercase tracking-wide">
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  placeholder="Bibek Soti"
-                  name='fullName'
-                  value={formData.fullName}
-                  onChange={changeHandler}
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#ecb403] focus:border-transparent"
-                />
-              </div>
 
               {/* Email */}
               <div>
@@ -124,39 +103,6 @@ export default function SignupIn() {
                 </div>
               </div>
 
-              {/* Confirm Password */}
-              <div>
-                <label className="block text-xs font-medium text-gray-700 mb-2 uppercase tracking-wide">
-                  Confirm Password
-                </label>
-                <div className="relative">
-                  <input
-                    type={showConfirmPassword ? 'text' : 'password'}
-                    placeholder="••••••••••"
-                    name='confirmPassword'
-                    value={formData.confirmPassword}
-                    onChange={changeHandler}
-                    className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#ecb403]  focus:border-transparent pr-12"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                    className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-                  >
-                    {showConfirmPassword ? (
-                      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
-                      </svg>
-                    ) : (
-                      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                      </svg>
-                    )}
-                  </button>
-                </div>
-              </div>
-
               {/* Terms Checkbox */}
               <div className="flex items-start gap-3 pt-2">
                 <input
@@ -167,15 +113,7 @@ export default function SignupIn() {
                   className=" cursor-pointer mt-1 w-4 h-4 text-teal-600 border-gray-300 rounded focus:ring-teal-500"
                 />
                 <label htmlFor="terms" className="text-sm text-gray-600">
-                  I agree to the{' '}
-                  <a href="#" className="text-[#ecb403] hover:underline">
-                    Terms of Service
-                  </a>{' '}
-                  and{' '}
-                  <a href="#" className="text-[#ecb403] hover:underline">
-                    Privacy Policy
-                  </a>
-                  .
+                  Remember Me
                 </label>
               </div>
 
@@ -190,10 +128,9 @@ export default function SignupIn() {
 
               {/* Sign In Link */}
               <div className="text-center pt-4">
-                <span className="text-gray-600">Have an account? </span>
-
-                <Link to="/signin" className="text-[#ecb403] hover:underline font-medium" >
-                Sign In
+                <span className="text-gray-600">Don't have an account? </span>
+                <Link to="/signup" className="text-[#ecb403] hover:underline font-medium" >
+                Sign Up
                 </Link>
               </div>
             </div>
