f94513f3676da059810c3bb1952d74b5ab49e8c0
beebek1 <beebek990@gmail.com>
2026-02-15 18:44:46 +0545

    chore: remove legacy backend structure and update package configuration

--- backend/routes/movieRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const {addMovie, addShowTime, deleteMovie} = require('../controllers/adminController');
-
-router.post("/addmovie", addMovie);
-router.post("/addshowtime", addShowTime);
-router.delete("/:id", deleteMovie);
-
-module.exports = router;
