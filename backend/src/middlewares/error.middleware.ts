6da478b0543506ab00e57bc9815333ccb61154d7
beebek1 <beebek990@gmail.com>
2026-03-30 02:23:32 +0545

    added global error handler

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,23 @@
+import type { Response, Request, NextFunction } from "express";
+import { ApiError } from "../utils/apiError.js";
+
+export const errorHandler = (
+    err : any,
+    req : Request,
+    res : Response,
+    next : NextFunction
+) => {
+    let{ statusCode, message, errors } = err;
+
+    if(!(err instanceof ApiError)){
+        statusCode = 500,
+        message = err.message || "Internal Server Error",
+        errors = [];
+    }
+
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        errors,
+    })
+}
