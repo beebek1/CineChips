ce200ebf3fe066efee7c6b5c460334c009f9b307
beebek1 <beebek990@gmail.com>
2026-04-04 17:24:39 +0545

    rewrote route in modular format feat showtimes

---
+++ backend/src/modules/showtime/showtime.route.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {authenticate} from "../../middlewares/auth.middleware.js"
+import {authorize} from "../../middlewares/role.middleware.js"
+import {
+  addShowtime,
+  getShowtime,
+  getShowtimeByMovieId,
+  deleteShowtime
+} from "./showtime.controller.js";
+import { validator } from "../../middlewares/validator.middleware.js";
+import { showtimeSchema } from "./showtime.validator.js";
+
+const router = Router();
+
+router.post("/showtimes",authenticate, authorize("org"),validator(showtimeSchema), addShowtime);
+router.get("/showtimes", authenticate, authorize("org"), getShowtime);
+router.get("/showtimes/:id", authenticate, authorize("user"), getShowtimeByMovieId);
+router.delete("/showtimes/:id", authenticate, authorize("org"), deleteShowtime);
+
+export default router;
+
+// router.post("/showtimes", addShowTime);
+// router.get("/get-showtime", getShowTimes);
+// router.delete("/showtime/delete/:id", deleteShowTime);
