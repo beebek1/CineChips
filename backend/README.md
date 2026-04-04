654a16616093af8f9a510c04f4edce716d59efba
beebek1 <beebek990@gmail.com>
2026-04-04 21:54:02 +0545

    finalized app and server files structure

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# CineChips-backend
-Backend for a movie ticket booking platform with seat maps, timing, and booking flow
---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import "./utils/crons.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import rootRouter from "./modules/main.router.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+
+const app = express();
+
+// Uploads stuff
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to CineChips API" });
+});
+app.use("/api", rootRouter);
+
+// Error Handler
+app.use(errorHandler);
+
+export default app;
