5e3b375bf695ae4dae7844b75245aa5e1b13cdb5
beebek1 <beebek990@gmail.com>
2026-04-08 07:02:53 +0545

    refactor(client): update main app component and hydration logic

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,12 +1,19 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import ScrollToTop from "../shared/components/ScrollToTop";
+import AppRoutes from "./routes/AppRoutes";
 
-import './App.css'
-
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-    </>
-  )
-}
+    <BrowserRouter>
+      <ScrollToTop />
+      <Toaster position="top-right" />
+      <div className="bg-[#080808] min-h-screen text-white">
+        <AppRoutes />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
